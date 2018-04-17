4ae4902ef20f1926ae295dfdd95e5ddff59d94cc
SZIGETVÁRI Áron <aron.szigetvari@dream.hu>
2018-04-17 19:13:59 +0200

    Git wrapper

--- experiment.php
+++ experiment.php
@@ -3,9 +3,31 @@
 use SebastianBergmann\Diff\Parser;
 use SebastianBergmann\Git\Git;
 
+$repo = $argv[1];
+$diffParam = $argv[2];
+
 require_once 'vendor/autoload.php';
 
-exec('git diff --no-ext-diff -U0 *.php', $outputLines);
+$cwd = getcwd();
+
+$cdCommand = 'cd ' . escapeshellarg($repo);
+$command = 'giti dff --no-ext-diff -U0 ' . $diffParam . ' *.php';
+$command = 'git status';
+
+exec($cdCommand);
+exec($command, $outputLines);
+
+print_r($outputLines);
+die;
+//$descriptorspec = array(
+//    0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
+//    1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
+//    2 => array("file", "/tmp/error-output.txt", "a") // stderr is a file to write to
+//);
+//
+//$process = proc_open($command, $descriptorspec, $pipes, $repo);
+//if (is_resource($process))
+chdir($cwd);
 
 $parser = new Parser;
 $diff = $parser->parse(implode("\n", $outputLines));
---
+++ src/TestSelector.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aron
+ * Date: 2018. 04. 16.
+ * Time: 23:51
+ */
+
+namespace AronSzigetvari\TestSelector;
+
+
+class TestSelector
+{
+    public function selectTests()
+}
